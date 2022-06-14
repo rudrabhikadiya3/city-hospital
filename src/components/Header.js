ca36fdf7912b785f8b0a0ed97f4bc436c1ba1c76
rudrabhikadiya3 <rudra.rnw@gmail.com>
2022-06-14 16:00:47 +0530

    login and signup condition rend

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,9 +38,9 @@ function Header(props) {
                     </nav>
                     <a href="./pages/appointment.html" className="appointment-btn scrollto"><span className="d-none d-md-inline">Make an</span>
                         Appointment</a>
-                    <a href="#" className="appointment-btn scrollto">
+                    <NavLink to="/login" className="appointment-btn scrollto">
                         <span className="d-none d-md-inline">Login/ Signup</span>
-                    </a>
+                    </NavLink>
                 </div>
             </header>
         </div>
